2472c84a97632366917bcb2589b57d9bd385cfaa
Emanuel Oliveira <emanuelgama219@gmail.com>
2026-04-28 13:00:19 -0400

    modified teste.c

--- teste.c
+++ teste.c
@@ -26,12 +26,26 @@ typedef struct{
 }Discente;
 
 int main(){
-Curso ads = {"Analize e Desenvolvimento de Sistemas", 2345,2800,30,20};
-Turma ano2026_1 = {101,2026,1300,&ads};
-Discente pedro = {"Pedro Henrique",432987,18,10,8,&ano2026_1};
+Curso cursos[3];
+Turma turmas[3];
+Discente discentes[3];
 
-printf("%s\n",pedro.nome);
-printf("numero da turma de pedro %d", pedro.turma->numero);
+cursos[0] = (Curso){"Analize e Desenvolvimento de Sistemas", 1001,2800,30,20};
+cursos[1] = (Curso){"Licenciatura em Quimica", 1002,3200,30,20};
+cursos[2] = (Curso){"Licenciatura em Matematica", 1003,3200,30,20};
+
+turmas[0] = (Turma){101,2026,1300,&cursos[0]};
+turmas[1] = (Turma){102,2026,1800,&cursos[1]};
+turmas[2] = (Turma){103,2026,1800,&cursos[2]};
+
+discentes[0] = (Discente){"Pedro Henrique",432987,18,10,8,&turmas[0]};
+discentes[1] = (Discente){"Maria Pinheiro",124567,21,9,6,&turmas[1]};
+discentes[2] = (Discente){"Luis Gonsalves",546723,32,7,9,&turmas[2]};
+
+printf("%s\n",discentes[0].nome);
+printf("numero da turma de pedro %d\n", discentes[0].turma->numero);
+printf("nome do curso de pedro %s", discentes[0].turma->curso->nome);
+
 
 return 0;
 }
